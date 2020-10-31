aed644ea5ac3e5f0e2cf5985d1d799df5fa68dbc
aidanmatheney <aidanmatheney@gmail.com>
2020-10-30 21:37:32 -0500

    Web - Add Destroyed$ service

---
+++ web/projects/inventory-system-web/src/app/service/destroyed$.service.ts
@@ -0,0 +1,16 @@
+import {Injectable, OnDestroy} from '@angular/core';
+import {Observable, ReplaySubject} from 'rxjs';
+
+@Injectable()
+export class Destroyed$ extends Observable<void> implements OnDestroy {
+  private readonly destroyed$ = new ReplaySubject<void>(1);
+
+  public constructor() {
+    super((subscriber) => this.destroyed$.subscribe(subscriber))
+  }
+
+  public ngOnDestroy() {
+    this.destroyed$.next();
+    this.destroyed$.complete();
+  }
+}
